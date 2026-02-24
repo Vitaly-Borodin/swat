b706ae8a87d58a0809d44d2a07e39c1e3c7c5701
Vitaly Borodin <026rus@gamil.com>
2026-02-23 18:03:31 -0600

    clean up

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('swat');
+  protected readonly title = signal("S.W.A.T");
 }
