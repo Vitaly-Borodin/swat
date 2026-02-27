cb40c5b8afd83e41c68f2c432813dc076c686116
Vitaly Borodin <026rus@gamil.com>
2026-02-27 14:58:35 -0600

    Adding new title

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,4 +8,8 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal("S.W.A.T");
+
+  public changeTitle(): void {
+    this.title.set("S.W.A.T - South West Association of Tournaments");
+  }
 }
